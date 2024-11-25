3576b4f8ec926d6fe9cb67561135bdec10878a7f
wooseok <youkn0wthat@naver.com>
2024-11-25 22:28:26 +0900

    내 db에 회원가입 정보 저장

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/users";
+const API_URL = "https://moneyfulpublicpolicy.co.kr";
 
 export const register = async (userData) => {
   try {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,21 +4,21 @@ import { login } from "../api/auth";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [userId, setUserId] = useState("");
-  const [password, setPassword] = useState("");
+  // const [userId, setUserId] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const handleLogin = async () => {
-    // try {
-    const userData = { id: userId, password };
-    const response = await login(userData);
+  // const handleLogin = async () => {
+  //   // try {
+  //   const userData = { id: userId, password };
+  //   const response = await login(userData);
 
-    if (response.length > 0) {
-      navigate("/");
-    } else {
-      alert("아이디 또는 비밀번호가 일치하지 않습니다.");
-      // }
-    }
-  };
+  //   if (response.length > 0) {
+  //     navigate("/");
+  //   } else {
+  //     alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+  //     // }
+  //   }
+  // };
 
   const handleSignup = () => {
     navigate("/signup");
@@ -28,10 +28,10 @@ const Login = () => {
     <>
       <form>
         <label>로그인</label>
-        <input type="text" value={} onChange={}></input>
+        <input type="text"></input>
         <label>패스워드</label>
-        <input type="password" value={} onChange={}></input>
-        <button onClick={handleLogin}>로그인</button>
+        <input type="password"></input>
+        <button>로그인</button>
         <button onClick={handleSignup}>회원가입</button>
       </form>
     </>
