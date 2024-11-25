4fe780306cb5898655a1da04590aaa3d2be3ddbe
wooseok <youkn0wthat@naver.com>
2024-11-25 11:46:58 +0900

    초기 설정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+    </div>
+  );
+};
+
+export default Login;
