913368eb0d1b7d43a9ad66b7bd7224e9ed368545
wooseok <youkn0wthat@naver.com>
2024-11-28 10:09:23 +0900

    안쓰는 페이지 삭제

--- src/components/LoginComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoginComponent = () => {
-  return <div>LoginComponent</div>;
-};
-
-export default LoginComponent;
--- src/components/SignUpComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignUpComponent = () => {
-  return <div>SignUpComponent</div>;
-};
-
-export default SignUpComponent;
