d61f1536c8370d5b79bfb329f0d27e7ec0d7a3d9
wooseok <youkn0wthat@naver.com>
2024-11-28 12:36:41 +0900

    완성

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/testResults";
+const API_URL = "https://separated-buttery-pajama.glitch.me/testResults";
 
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
